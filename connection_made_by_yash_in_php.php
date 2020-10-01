4ed2ecb04f1f932e49a1b2308cce83982608d08c
Yash Mittal <47789628+mittalyash394@users.noreply.github.com>
2020-10-01 18:08:08 +0530

    Add files via upload

---
+++ connection_made_by_yash_in_php.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+
+	 Welcome <?php echo $_POST['Name'];?><br>
+	 Your email address is <?php echo $_POST['Email'];?><br>
+	 Your contact no is <?php echo $_POST['Phone_no'];?><br>
+	 Gender is <?php echo $_POST['gender'];?><br>
+	</body>
+</html>
